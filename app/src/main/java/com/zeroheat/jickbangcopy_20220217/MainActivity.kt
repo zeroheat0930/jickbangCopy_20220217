4ec5431570e4ff9e005f83d4b6feddc8b75c86a2
zeroheat0930 <dongjunkorea@naver.com>
2022-02-17 14:33:35 +0900

    Initial commit

---
+++ app/src/main/java/com/zeroheat/jickbangcopy_20220217/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zeroheat.jickbangcopy_20220217
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
